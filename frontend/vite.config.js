a37bd179918cd4332504cee7a662a9d418946afb
Piyush <piyushrs007@gmail.com>
2026-06-05 13:38:51 +0530

    vite updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+/*import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 
@@ -7,5 +7,17 @@ export default defineConfig({
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
+  ],
+})
+*/
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
   ],
 })
